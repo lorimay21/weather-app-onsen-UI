f769dbe231db091e5f005e2c14878d67c2b14630
Lori May De Guzman <dlorimay21@gmail.com>
2021-02-28 22:03:05 +0800

    Vue app using Monaca Onsen UI

---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  WEATHER_API_BASE_URL: '""',
+  WEATHER_API_KEY: '""'
+}
---
+++ config/staging.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"test"',
+  WEATHER_API_BASE_URL: '""',
+  WEATHER_API_KEY: '""'
+}
